b53ab06d177f819b8e4bb30f1ccff69ab59c998c
SolidSnake <solid@snake>
2022-10-27 19:14:52 +0000

    fix Buy endpoint and add Claim endpoint

--- front-end/pages/raffle.tsx
+++ front-end/pages/raffle.tsx
@@ -7,13 +7,14 @@ import { useState, useEffect } from 'react'
 import { getAssets } from "../utils/cardano";
 import NftGrid from "../components/NftGrid";
 import initLucid from '../utils/lucid'
-import { Lucid, TxHash, Lovelace, Constr, SpendingValidator, Data, utf8ToHex, Script, Datum, datumJsonToCbor, toHex, sha256, concat } from 'lucid-cardano'
+import { Lucid, TxHash, Lovelace, Constr, SpendingValidator, Data, utf8ToHex, Script, MintingPolicy, Datum, datumJsonToCbor, toHex, sha256, concat, Unit, hexToUtf8 } from 'lucid-cardano'
 import * as helios from '@hyperionbt/helios'
 import {exportedForTesting as helios_internal} from '@hyperionbt/helios'
 import { min_fee } from 'lucid-cardano/types/src/core/wasm_modules/cardano_multiplatform_lib_web/cardano_multiplatform_lib'
 import { sign } from 'crypto'
 import { setPriority } from 'os'
 import { mintTicketValidator, lotteryValidator } from '../utils/validators'
+import { Console } from 'console'
 
 
 const Raffle: NextPage = () => {
@@ -23,6 +24,7 @@ const Raffle: NextPage = () => {
   const [script, setScript] = useState<SpendingValidator>()
   const [scriptAddress, setScriptAddress] = useState("")
   const [rafflePolicyId, setRafflePolicyId] = useState("")
+  const [rafflePolicy, setRafflePolicy]= useState<Script>({type: "Native", script:""})
   const [ticketPrice, setTicketPrice] = useState(10000000)
   const [randomSeed, setRandomSeed ] = useState("adadadsds")
   const [maxTicket, setMaxTicket] = useState(3)
@@ -31,28 +33,33 @@ const Raffle: NextPage = () => {
 
   useEffect(() => {
     if (lucid) {
-      const thisScript: SpendingValidator = {
-        type: "PlutusV1",
-        script: JSON.parse(
-          helios.Program.new(`
-          spending matching_pubKeyHash
-          struct Datum {
-              owner: PubKeyHash
+      /**
+      * One shot function to Initialise raffle policy.
+      */
+      const initRafflePolicy = async () => {
+        const {paymentCredential} = lucid.utils.getAddressDetails(await lucid.wallet.address());
+        const mintingPolicy = lucid.utils.nativeScriptFromJson(
+          {
+              type: "sig",
+              keyHash: paymentCredential?.hash
           }
-          struct Redeemer {
-              owner: PubKeyHash
-          }
-          func main(datum : Datum, redeemer: Redeemer) -> Bool {datum.owner == redeemer.owner}
-      `).compile().serialize(),
-        ).cborHex,
-      };
-      setScript(thisScript)
-      setScriptAddress(lucid.utils.validatorToAddress(thisScript))
+        )
+        console.log('minting policy : ', mintingPolicy)
+        const policyId = lucid.utils.mintingPolicyToId(
+            mintingPolicy,
+        );
+        setRafflePolicyId(policyId)
+        setRafflePolicy(mintingPolicy)
+      }
+      initRafflePolicy()
     } else {
       initLucid(walletStore.name).then((Lucid: Lucid) => { setLucid(Lucid) })
     }
   }, [lucid])
   
+  /**
+    * Convert seconds to miliseconds.
+    */
   const toMiliseconds = (seconds : number) => {
     return Math.floor(seconds * 1000)
   }
@@ -81,96 +88,41 @@ const Raffle: NextPage = () => {
   const mintRaffle = async () => {
     if (lucid) {
         console.log("minting raffle")
-        const {paymentCredential} = lucid.utils.getAddressDetails(await lucid.wallet.address());
+        console.log('raffle name : ', utf8ToHex("RaffleNFT #1"))
 
-        // const mintingPolicy = lucid.utils.nativeScriptFromJson(
-        //     {
-        //         type: "all",
-        //         scripts: [
-        //             { type: "sig", keyHash: paymentCredential?.hash },
-        //             {
-        //                 type: "before",
-        //                 slot: lucid.utils.unixTimeToSlot(Date.now() + 1000000),
-        //             },
-        //         ],
-        //     },
-        // )
-
-        const mintingPolicy = lucid.utils.nativeScriptFromJson(
-            {
-                type: "sig",
-                keyHash: paymentCredential?.hash
-            }
-        )
-
-        console.log('minting policy : ', mintingPolicy)
-        const policyId = lucid.utils.mintingPolicyToId(
-            mintingPolicy,
-          );
-        console.log('policy id : ', policyId)
-        setRafflePolicyId(policyId)
-
-
-        const unit = policyId + utf8ToHex("RaffleNFT #1");
-
-        console.log('Raffle NFT : ', unit)
+        const unit = rafflePolicyId + utf8ToHex("RaffleNFT #1");
+        console.log('Raffle Unit : ', unit)
         
         const asset = {[unit]: BigInt(1)}
-
         console.log('Asset :', asset)
+
         const tx = await lucid.newTx().
                          mintAssets(asset).
                          validTo(Date.now()+toMiliseconds(100)).
-                         attachMintingPolicy(mintingPolicy).
+                         attachMintingPolicy(rafflePolicy).
                          complete();
         const signedTx = await tx.sign().complete();
         const txHash = await signedTx.submit();
         console.log('Transaction submited:', txHash)
-
     }
   }
 
 
   const burnRaffle = async () => {
     if (lucid) {
-        console.log("minting raffle")
-        const {paymentCredential} = lucid.utils.getAddressDetails(await lucid.wallet.address());
-
-        // const mintingPolicy = lucid.utils.nativeScriptFromJson(
-        //     {
-        //         type: "all",
-        //         scripts: [
-        //             { type: "sig", keyHash: paymentCredential?.hash },
-        //             {
-        //                 type: "before",
-        //                 slot: 11033009,
-        //             },
-        //         ],
-        //     },
-        // )
-
-        const mintingPolicy = lucid.utils.nativeScriptFromJson(
-            {
-                type: "sig",
-                keyHash: paymentCredential?.hash
-            }
-        )
-
-        //const policyId = 'ba6bdf86608cb4cd0d2bfaf7bfdc68720e5e88b655e4cd4302f4758d'
-        console.log('policy id : ', rafflePolicyId)
+        console.log("burning raffle")
 
         const unit = rafflePolicyId + utf8ToHex("RaffleNFT #1");
-
-        console.log('Raffle NFT : ', unit)
-        
+        console.log('Raffle Unit : ', unit)
+    
         const asset = {[unit]: BigInt(-1)}
-
         console.log('Asset :', asset)
+
         const tx = 
             await lucid.newTx().
             mintAssets(asset).
             validTo(Date.now()+ toMiliseconds(100)).
-            attachMintingPolicy(mintingPolicy).
+            attachMintingPolicy(rafflePolicy).
             complete();
         const signedTx = await tx.sign().complete();
         const txHash = await signedTx.submit();
@@ -181,10 +133,10 @@ const Raffle: NextPage = () => {
   const startRaffle = async () => {
     if (lucid) {
         console.log("starting raffle")
-        console.log(rafflePolicyId)
+        console.log('raffle policy id : ', rafflePolicyId)
         const lotteryValidatorAdd = lucid.utils.validatorToAddress(lotteryValidator)
-        console.log(lotteryValidatorAdd)
-        console.log(lucid.utils.validatorToAddress(mintTicketValidator))
+        console.log('lottery validator address : ', lotteryValidatorAdd)
+        console.log('mint ticket validator address : ', lucid.utils.validatorToAddress(mintTicketValidator))
         const lotteryTicketPrice = 10000000;
         const lotteryRandomSeed = "adadadsds"
         const lotteryMaxTicket = 3;
@@ -208,13 +160,13 @@ const Raffle: NextPage = () => {
             lotteryTickets,
             lotteryIntervals
         ])
-        console.log(lotteryDatum)
+        console.log('lottery datum (PlutusData) : ', lotteryDatum)
 
         const datum = Data.to(lotteryDatum) // serialise to CBOR
-        console.log(datum)
+        console.log('lottery datum (CBOR) : ', datum)
         
         //console.log(lucid.utils.unixTimeToSlot(Date.now()))
-        const unit = rafflePolicyId + utf8ToHex ("RaffleNFT #1")
+        const unit : Unit = rafflePolicyId + utf8ToHex ("RaffleNFT #1")
         const utxos = await lucid.utxosAtWithUnit(await lucid.wallet.address(), unit)
 
         if (utxos.length > 0) {
@@ -231,7 +183,7 @@ const Raffle: NextPage = () => {
             console.log('Transaction submited:', txHash)
 
         } else {
-            console.log('Raffle not found in wallet address')
+            alert('Raffle not found in wallet address')
         }
 
     }
@@ -240,25 +192,18 @@ const Raffle: NextPage = () => {
   const buyTicket = async () => {
     if (lucid){
         const lotteryValidatorAdd = lucid.utils.validatorToAddress(lotteryValidator)
-        console.log('lottery script address : ', lotteryValidatorAdd)
-        const unitRaffle = rafflePolicyId + utf8ToHex ("RaffleNFT #1")
+        console.log('lottery validator address : ', lotteryValidatorAdd)
+        const unitRaffle : Unit = rafflePolicyId + utf8ToHex ("RaffleNFT #1")
         const assetRaffle = {[unitRaffle]: BigInt(1)}
         const scriptUtxos = await lucid.utxosAtWithUnit(lotteryValidatorAdd, unitRaffle);
         const walletUtxos = await lucid.wallet.getUtxos()
 
         if (walletUtxos.length && scriptUtxos.length > 0) {
             console.log('wallet txHash : ', walletUtxos[0].txHash)
-            console.log('outputIndex', walletUtxos[0].outputIndex)
+            console.log('outputIndex : ', walletUtxos[0].outputIndex)
             const walletTxHash = walletUtxos[0].txHash
             const walletOutputIndex = walletUtxos[0].outputIndex
-            //console.log(helios.bytesToHex([2]))
-            //console.log(helios.bytesToHex([2,97,98,99]))
-            //console.log(helios.bytesToHex([255]))
-            //console.log(toHex(new Uint8Array([1])))
-            //console.log(toHex(new Uint8Array([1])) + '34fc86e20ea0a1984e27c7594853bb2b25f32a61d0448fed85884d6f3638784b')
-            //console.log(helios_internal.Crypto.sha2_256())
-            //const txhash = new Uint8Array (helios_internal.stringToBytes(walletTxHash)) // lucid
-            //const txHashBytes = helios_internal.stringToBytes(walletTxHash) // helios
+
             const txHashBytes = helios.hexToBytes(walletTxHash) // helios
             console.log('txHashBytes : ', txHashBytes)
             //const id = new Uint8Array([walletOutputIndex])  // lucid
@@ -273,10 +218,10 @@ const Raffle: NextPage = () => {
             const hashedTokenName = helios.bytesToHex(hashedTokenNameBytes)
             console.log('hashedTokenName : ', hashedTokenName)
 
-            console.log('utxo found in lottery validator address : ', scriptUtxos)
+            console.log('utxo found in lottery validator : ', scriptUtxos)
             if (scriptUtxos[0].datumHash) {
                 const utxoDatum = Data.from (await lucid.datumOf(scriptUtxos[0])) // from CBOR to PlutusData/Json
-                console.log('datum found : ', utxoDatum)
+                console.log('script datum found(LotteryDatum) : ', utxoDatum)
                 const newDatum = {
                     lotteryTicketPrice : utxoDatum.fields[0],
                     lotteryRandomSeed : utxoDatum.fields[1],
@@ -286,53 +231,119 @@ const Raffle: NextPage = () => {
                     lotteryTickets : utxoDatum.fields[5],
                     lotteryIntervals : utxoDatum.fields[6]
                 }
-                console.log('new datum : ', newDatum)
                 const newLotteryDatum = new
                     Constr (0, [
                         newDatum.lotteryTicketPrice,
-                        newDatum.lotteryRandomSeed,
+                        newDatum.lotteryRandomSeed + utf8ToHex("s"),
                         newDatum.lotteryMaxTicket,
                         newDatum.lotterySoldTicket + BigInt(1),
                         newDatum.lotteryMinimumHash,
                         newDatum.lotteryTickets.concat(hashedTokenName),
                         newDatum.lotteryIntervals
                     ])
-                console.log('new lottery datum : ', newLotteryDatum)
+                console.log('new script datum(LotteryDatum): ', newLotteryDatum)
                 const datum = Data.to(newLotteryDatum)
-                console.log('datum :', datum)
+                console.log('new script datum(CBOR):', datum)
                 const validatorRedeemer = Data.to(new Constr (0,[])) // Buy redeemer
-                console.log('validatorRedeemer : ', validatorRedeemer)
+                console.log('validatorRedeemer(Buy) : ', validatorRedeemer)
                 const mintTicketPolicyId = lucid.utils.mintingPolicyToId(mintTicketValidator)
                 const unitTicket = mintTicketPolicyId + hashedTokenName
                 const assetTicket = {[unitTicket]: BigInt(1)}
-                console.log(assetTicket)
-                const walletTxHashData = new Constr (0, [walletTxHash])
-                const mintingRedeemerData = new Constr (0, [ walletTxHashData , BigInt(walletOutputIndex)]) // TxOutRef
+                console.log('assetTicket : ', assetTicket)
+                const walletTxHashData = new Constr (0, [walletTxHash]) // refers to TxId Constructor in PlutusTx
+                const mintingRedeemerData = new Constr (0, [ walletTxHashData , BigInt(walletOutputIndex)]) // refers TxOutRef constructor in PlutusTx
                 const mintingRedeemer = Data.to(mintingRedeemerData) // TxOutRef to CBOR
-                console.log(mintingRedeemerData)
+                console.log('mintingRedeemerData(TxOutRef) : ', mintingRedeemerData)
                 const lovelace : Lovelace = BigInt(10000000)
-                console.log(mintingRedeemer)
-                // TOdo add pay to wallet when minting
-                const tx = await lucid.newTx()
-                           .collectFrom([scriptUtxos[0]],validatorRedeemer)
-                           .attachSpendingValidator(lotteryValidator)
-                           .attachMintingPolicy(mintTicketValidator)
-                           .mintAssets(assetTicket, mintingRedeemer)
-                           .payToContract(lotteryValidatorAdd, datum,{lovelace, [unitRaffle]: BigInt(1)})
-                           .validTo(Date.now())
-                           .complete()
+                //const lovelace  = ""
+                console.log('mintingRedeemer(CBOR) : ', mintingRedeemer)
+                try {
+                  const tx = await lucid.newTx()
+                             .collectFrom([scriptUtxos[0],walletUtxos[0]],validatorRedeemer)
+                             .attachSpendingValidator(lotteryValidator)
+                             .attachMintingPolicy(mintTicketValidator)
+                             .mintAssets(assetTicket, mintingRedeemer)
+                             .payToContract(lotteryValidatorAdd, datum,{lovelace, [unitRaffle]: BigInt(1)})
+                             .validTo(Date.now() + toMiliseconds(100))
+                             .complete()
+                  const signedTx = await tx.sign().complete()
+                  const txHash = signedTx.submit()
+                  console.log('Transaction submited:', txHash)
+                } catch (err) {
+                  alert(err)
+                }
+                // Just minting ticket policy to test validation
+                //const tx = await lucid.newTx()
+                //          .collectFrom([walletUtxos[0]])
+                //          .attachMintingPolicy(mintTicketValidator)
+                //          .mintAssets(assetTicket, mintingRedeemer)
+                //          .complete()
                 //console.log(tx)
-                const signedTx = await tx.sign().complete()
-                   
-                const txHash = signedTx.submit()
-                console.log('Transaction submited:', txHash)
             }
         }
         else {
-            console.log('no utxos found in loterry validator address : ', lotteryValidatorAdd)
+            alert('no utxos found in loterry validator address : ' + lotteryValidatorAdd)
         }
         
   }}
+
+  const claim = async () => {
+    if (lucid){
+      const lotteryValidatorAdd = lucid.utils.validatorToAddress(lotteryValidator)
+      const unitRaffle : Unit = rafflePolicyId + utf8ToHex ("RaffleNFT #1")
+      const mintTicketPolicyId = lucid.utils.mintingPolicyToId(mintTicketValidator)
+      const scriptUtxos = await lucid.utxosAtWithUnit(lotteryValidatorAdd, unitRaffle);
+      const walletUtxos = await lucid.wallet.getUtxos()
+      console.log(walletUtxos)
+      const ticketAssets = walletUtxos.map(utxo => (Object.keys(utxo.assets))).flat()
+                          .filter(asset => asset.includes(mintTicketPolicyId))
+      console.log('tickets found :', ticketAssets)
+
+      
+      console.log("Claiming")
+      if (walletUtxos.length && scriptUtxos.length > 0) {
+        if (scriptUtxos[0].datumHash) {
+          const utxoDatum = Data.from (await lucid.datumOf(scriptUtxos[0])) // from CBOR to PlutusData/Json
+          console.log('script datum found(LotteryDatum) : ', utxoDatum)
+          const newDatum = {
+            lotteryTicketPrice : utxoDatum.fields[0],
+            lotteryRandomSeed : utxoDatum.fields[1],
+            lotteryMaxTicket : utxoDatum.fields[2],
+            lotterySoldTicket : utxoDatum.fields[3],
+            lotteryMinimumHash : utxoDatum.fields[4],
+            lotteryTickets : utxoDatum.fields[5],
+            lotteryIntervals : utxoDatum.fields[6]
+          }
+          const tokenName = ticketAssets[0].slice(56)
+          const tokenNameBytes = helios.hexToBytes(tokenName)
+          console.log('tokenName(Bytes):', tokenNameBytes)
+          //sha2_256 (appendByteString ticketName $ consByteString soldTickets raffleSeed)
+          const pseudoRandomNumber = [Number(newDatum.lotterySoldTicket)].concat(helios.hexToBytes(newDatum.lotteryRandomSeed))
+          console.log('Pseudo Random number(Bytes) :',pseudoRandomNumber)
+          const claimMinimunHashBytes = helios_internal.Crypto.sha2_256(tokenNameBytes.concat(pseudoRandomNumber))
+          console.log('claimMinimumHashBytes(Bytes):', claimMinimunHashBytes)
+          const claimMinimumHash = helios.bytesToHex(claimMinimunHashBytes)
+          console.log('claimMinimumHash(Hex) :',claimMinimumHash )
+
+          const newLotteryDatum = new
+                    Constr (0, [
+                        newDatum.lotteryTicketPrice,
+                        newDatum.lotteryRandomSeed,
+                        newDatum.lotteryMaxTicket,
+                        newDatum.lotterySoldTicket,
+                        claimMinimumHash,
+                        newDatum.lotteryTickets,
+                        newDatum.lotteryIntervals
+                    ])
+          console.log('new script datum(LotteryDatum): ', newLotteryDatum)
+          
+
+        }
+      } else {
+        alert('no utxos found in loterry validator address : ' + lotteryValidatorAdd)
+      }
+    }
+  }
 
 // <input  name="ticket price" defaultValue={ticketPrice} onChange={e => setTicketPrice(parseInt (e.target.value))} />
   return (
@@ -360,6 +371,7 @@ const Raffle: NextPage = () => {
         <button className="btn btn-secondary m-5" onClick={() => { getUtxos() }}>Get Utxo</button>
         <button className="btn btn-secondary m-5" onClick={() => { startRaffle() }}>Start Raffle</button>
         <button className="btn btn-secondary m-5" onClick={() => { buyTicket() }}>Buy Ticket</button>
+        <button className="btn btn-secondary m-5" onClick={() => { claim() }}>Claim</button>
       </div>
     </div>
   )
