c0c16daf7899a08334c18de69cbe0e851e3a606e
SolidSnake <solid@snake>
2022-11-04 16:19:39 +0000

    disable MintRaffle

--- app/Main.hs
+++ app/Main.hs
@@ -15,13 +15,14 @@ import qualified Data.ByteString.Char8 as BS
 import qualified Plutus.V1.Ledger.Value as Value
 
 main = do
-    [utxo, raffleName] <- getArgs
-    let utxo' = Utils.parseUTxO utxo
-        raffleName' = Value.tokenName $ BS.pack raffleName
-    print utxo'
-    print raffleName'
+    -- [utxo, raffleName] <- getArgs
+    -- let utxo' = Utils.parseUTxO utxo
+    --     raffleName' = Value.tokenName $ BS.pack raffleName
+    -- print utxo'
+    -- print raffleName'
     exist <- doesDirectoryExist "plutus-scripts"
     createDirectoryIfMissing exist "plutus-script"
-    Utils.writeValidator "plutus-scripts/mint-raffle.plutus" $ MintRaffle.getValidator $ MintRaffle.getScript $ MintRaffle.policy utxo' raffleName'
+    -- Temporary disable MintRaffle, because a Native Script is used in Lucid
+    -- Utils.writeValidator "plutus-scripts/mint-raffle.plutus" $ MintRaffle.getValidator $ MintRaffle.getScript $ MintRaffle.policy utxo' raffleName'
     Utils.writeValidator "plutus-scripts/lottery.plutus" $ Lottery.getValidator (MintTicket.getCurrencySymbol MintTicket.policy)
     Utils.writeValidator "plutus-scripts/mint-ticket.plutus" $ MintTicket.getValidator (MintTicket.getScript MintTicket.policy)
